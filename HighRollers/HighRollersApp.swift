01f390b01c9a16863183f151cd8e8e8e787e1355
tienbui <tienbui1508@gmail.com>
2023-07-17 21:28:47 +0930

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Tien Bui on 17/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
